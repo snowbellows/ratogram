ed6602b116d0ae6baeb5c7f519e11d3fe00f62a0
Alexis Halverson <alexhalverson@posteo.net>
2025-01-28 02:28:30 +1100

    add some default grams

---
+++ src/data/default.ts
@@ -0,0 +1,25 @@
+// 5x5
+
+export const mushroom = "grb1f3b1rf5rf5rb2f1b2rb2f1b2";
+
+export const bow = "grb2f1b2rb3f1b1rf5rb3f1b1rb2f1b2";
+
+export const sunfish = "grb1f1b3rb1f3b1rf5rb1f3b1rb1f1b3";
+
+export const heart = "grb1f1b1f1b1rf5rf5rb1f3b1rb2f1b2";
+
+export const lightning = "grb2f3rb1f2b2rf2b3rb2f2b1rb1f1b3";
+
+export const tulip = "grf4b1rf1b2f1b1rf1b2f1b1rb1f2b2rb2f1b2";
+
+export const tie = "grb1f1b1f1b1rb2f1b2rb2f1b2rb2f2b1rb2f1b2";
+
+export const key = "grb5rb1f1b3rf1b1f3rb1f1b2f1rb5";
+
+export const campfire = "grf1b1f3rb1f1b1f1b1rb2f1b2rb1f3b1rf1b1f1b1f1";
+
+export const flag = "grb1f2b2rb1f4rb1f3b1rb1f1b3rb1f1b3";
+
+export const star = "grf1b1f1b1f1rb1f2b2rf5rb1f3b1rb2f1b1f1";
+
+export const trafficController = "grb2f1b1f1rf5rb2f1b1f1rb1f2b1f1rf1b2f2";
