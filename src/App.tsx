5f5263844d83d514cfdf6e686395af4c93629f18
Alexis Halverson <alexhalverson@posteo.net>
2025-01-29 01:12:28 +1100

    feat(App): add key row

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Gram, GTile } from "./data";
 
 function App() {
-  const [gram, setGram] = useState(Gram.newBlank(5));
+  const [gram, setGram] = useState(Gram.newBlank(15));
 
-  const updateGramWithTile = ({
-    x,
-    y,
-    type,
-  }: {
-    x: number;
-    y: number;
-    type: GTile["type"];
-  }) => {
-    // TODO update Gram with Tile
-    let tiles = [...gram.tiles];
-    const tile = tiles[x][y];
+  const rowKeys = useMemo(
+    () => gram.rowBlocks().map((r) => r.filter((b) => b.type === "filled")),
+    [gram]
+  );
 
-    tiles[x][y] = { ...tile, type };
-
-    const newGram = new Gram(tiles);
+  const updateGramWithTile = ({ x, y }: { x: number; y: number }) => {
+    const newGram = gram.toggleTile({ x, y });
 
     history.pushState({}, "", `?g=${newGram.serialise()}`);
 
@@ -33,22 +24,23 @@ function App() {
     <>
       <div className="board">
         <div className="row-key">
-          {gram.row_blocks().map((r) => (
-            <div className="brow" key={`${r.map((b) => b.id).join("-")}`}>
-              {}
+          {rowKeys.map((r, i) => (
+            <div className="krow" key={`krow${i}${r.map((b) => b.id).join("-")}`}>
+              {r.map((b) => (
+                <p className="rkey" key={b.id}>{b.length}</p>
+              ))}
             </div>
           ))}
         </div>
         <div className="gram">
           {gram.rows().map((r, i) => (
-            <div className="grow" key={`${r.map((b) => b.id).join("-")}`}>
+            <div className="grow" key={`grow${i}${r.map((b) => b.id).join("-")}`}>
               {r.map((t, j) => (
                 <button
                   onClick={() =>
                     updateGramWithTile({
-                      x: i,
-                      y: j,
-                      type: t.type === "filled" ? "blank" : "filled",
+                      x: j,
+                      y: i,
                     })
                   }
                   className={`tile${t.type === "filled" ? " filled" : ""}`}
