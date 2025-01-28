f50dc206ab28fea9f54cacf7c822078727084fca
Alexis Halverson <alexhalverson@posteo.net>
2025-01-28 23:35:28 +1100

    fix(App): use keys

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,10 @@ function App() {
   }) => {
     // TODO update Gram with Tile
     let tiles = [...gram.tiles];
+    const tile = tiles[x][y];
 
-    tiles[x][y] = { type };
+    tiles[x][y] = { ...tile, type };
+
     const newGram = new Gram(tiles);
 
     history.pushState({}, "", `?g=${newGram.serialise()}`);
@@ -29,24 +31,33 @@ function App() {
 
   return (
     <>
-      <div className="gram">
-        {gram.rows().map((r, i) => (
-          <div className="grow" key={`row${i}`}>
-            {r.map((t, j) => (
-              <button
-                onClick={() =>
-                  updateGramWithTile({
-                    x: i,
-                    y: j,
-                    type: t.type === "filled" ? "blank" : "filled",
-                  })
-                }
-                className={`tile${t.type === "filled" ? " filled" : ""}`}
-                key={`${i}${j}${t.type}`}
-              ></button>
-            ))}
-          </div>
-        ))}
+      <div className="board">
+        <div className="row-key">
+          {gram.row_blocks().map((r) => (
+            <div className="brow" key={`${r.map((b) => b.id).join("-")}`}>
+              {}
+            </div>
+          ))}
+        </div>
+        <div className="gram">
+          {gram.rows().map((r, i) => (
+            <div className="grow" key={`${r.map((b) => b.id).join("-")}`}>
+              {r.map((t, j) => (
+                <button
+                  onClick={() =>
+                    updateGramWithTile({
+                      x: i,
+                      y: j,
+                      type: t.type === "filled" ? "blank" : "filled",
+                    })
+                  }
+                  className={`tile${t.type === "filled" ? " filled" : ""}`}
+                  key={t.id}
+                ></button>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
