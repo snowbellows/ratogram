e28df84417740bc830aab5fb9c19acd61eeae90b
Alex Halverson <alexhalverson@posteo.net>
2025-01-29 22:58:42 +1100

    chore(App): remove unused assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useMemo, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Gram, GTile } from "./data";
+import { Gram } from "./data";
 
 function App() {
   const [gram, setGram] = useState(Gram.newBlank(15));
