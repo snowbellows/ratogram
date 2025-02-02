9202aebd79b020c6c132f31a9d71e8090071368e
Alexis Halverson <alexhalverson@posteo.net>
2025-02-03 00:44:58 +1100

    chore(data): add roundtrip tests for default data

---
+++ src/data/default.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "vitest";
+import { ArrayGram } from "./gram/array-gram";
+import { defaultGramsSe } from "./default";
+
+for (let d in defaultGramsSe) {
+  const v = defaultGramsSe[d as keyof typeof defaultGramsSe];
+  test(`defaults roundtrip ${d}`, () => {
+    expect(ArrayGram.deserialise(v).serialise()).toBe(v);
+  });
+}
--- src/data/default.ts
+++ src/data/default.ts
@@ -1,27 +1,16 @@
-// 5x5
-
-export const rat = "grb4f1rb3f1b1rb1f1b2f1rf4b1rb1f1b1f1b1"
-
-export const mushroom = "grb1f3b1rf5rf5rb2f1b2rb2f1b2";
-
-export const bow = "grb2f1b2rb3f1b1rf5rb3f1b1rb2f1b2";
-
-export const sunfish = "grb1f1b3rb1f3b1rf5rb1f3b1rb1f1b3";
-
-export const heart = "grb1f1b1f1b1rf5rf5rb1f3b1rb2f1b2";
-
-export const lightning = "grb2f3rb1f2b2rf2b3rb2f2b1rb1f1b3";
-
-export const tulip = "grf4b1rf1b2f1b1rf1b2f1b1rb1f2b2rb2f1b2";
-
-export const tie = "grb1f1b1f1b1rb2f1b2rb2f1b2rb2f2b1rb2f1b2";
-
-export const key = "grb5rb1f1b3rf1b1f3rb1f1b2f1rb5";
-
-export const campfire = "grf1b1f3rb1f1b1f1b1rb2f1b2rb1f3b1rf1b1f1b1f1";
-
-export const flag = "grb1f2b2rb1f4rb1f3b1rb1f1b3rb1f1b3";
-
-export const star = "grf1b1f1b1f1rb1f2b2rf5rb1f3b1rb2f1b1f1";
-
-export const trafficController = "grb2f1b1f1rf5rb2f1b1f1rb1f2b1f1rf1b2f2";
+export const defaultGramsSe = {
+  // 5x5
+  rat: "grb4f1rb3f1b1rb1f1b2f1rf4b1rb1f1b1f1b1",
+  mushroom: "grb1f3b1rf5rf5rb2f1b2rb2f1b2",
+  bow: "grb2f1b2rb3f1b1rf5rb3f1b1rb2f1b2",
+  sunfish: "grb1f1b3rb1f3b1rf5rb1f3b1rb1f1b3",
+  heart: "grb1f1b1f1b1rf5rf5rb1f3b1rb2f1b2",
+  lightning: "grb2f3rb1f2b2rf2b3rb2f2b1rb1f1b3",
+  tulip: "grf4b1rf1b2f1b1rf1b2f1b1rb1f2b2rb2f1b2",
+  tie: "grb1f1b1f1b1rb2f1b2rb2f1b2rb2f2b1rb2f1b2",
+  key: "grb5rb1f1b3rf1b1f3rb1f1b2f1rb5",
+  campfire: "grf1b1f3rb1f1b1f1b1rb2f1b2rb1f3b1rf1b1f1b1f1",
+  flag: "grb1f2b2rb1f4rb1f3b1rb1f1b3rb1f1b3",
+  star: "grf1b1f1b1f1rb1f2b2rf5rb1f3b1rb2f1b1f1",
+  trafficController: "grb2f1b1f1rf5rb2f1b1f1rb1f2b1f1rf1b2f2",
+};
