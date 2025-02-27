85519fc9634e3dd4fc28b5c312a13f1d9ba6c7b7
Alex Halverson <alexhalverson@posteo.net>
2025-02-27 22:46:07 +1100

    chore(lint): run eslint

--- src/data/default.test.ts
+++ src/data/default.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "vitest";
 import { ArrayGram } from "./gram/array-gram";
 import { defaultGramsSe } from "./default";
 
-for (let d in defaultGramsSe) {
+for (const d in defaultGramsSe) {
   const v = defaultGramsSe[d as keyof typeof defaultGramsSe];
   test(`defaults roundtrip ${d}`, () => {
     expect(ArrayGram.deserialise(v).serialise()).toBe(v);
